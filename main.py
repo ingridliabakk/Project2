1d810a8e380a4543e9f742ffcc35e9cff48dc119
Tobias E <tseilertsen@gmail.com>
2020-10-02 15:27:14 +0200

    refactor

--- main.py
+++ main.py
@@ -6,43 +6,9 @@ import matplotlib.pyplot as plt
 import datetime
 import holidays
 from preprocessing import *
-plt.style.use('bmh')
+from patternvisualize import *
+plt.style.use('bmh') 
 
-
-
-
-def plot_features(file):
-    file = (pd.read_csv("data.csv"))
-    test = file
-    print(test)
-    fig, axs = plt.subplots(4)
-    fig.suptitle("all plots")
-    # axs[0].figure(figsize=(16,8))
-    #axs[0].title('Traffic', fontsize = 14)
-    #axs[0].xlabel('Timer', fontsize= 12)
-    #axs[0].ylabel('Volum Tot', fontsize = 12)
-    axs[0].title.set_text("num cars after first hours")
-    axs[0].plot(test['Volum totalt'], label='TOT')
-    axs[0].plot(test['Volum til SNTR'], label='SNTR')
-    axs[0].plot(test['Volum til DNP'], label='DNP')
-    axs[0].legend(loc='best')
-    # plotting something else
-    axs[1].title.set_text("different directions compared")
-    axs[1].scatter(test['Volum til SNTR'],
-                   test['Volum til DNP'], s=2.5, alpha=0.03)
-    axs[1].axis('equal')
-    # plotting something else
-    axs[2].title.set_text("volum etter time")
-    axs[2].scatter(test['Volum totalt'], test['Fra_time'],
-                   cmap='gist_rainbow', s=0.5)
-    # plotting something else
-    axs[3].title.set_text("volum etter time med begge veier")
-    axs[3].scatter(test['Volum til DNP'], test['Fra_time'],
-                   cmap='gist_rainbow', s=2.2, c='blue', alpha=0.03)
-    axs[3].scatter(test['Volum til SNTR'], test['Fra_time'],
-                   cmap='gist_rainbow', s=2.2, c='green', alpha=0.03)
-
-    plt.show()
 
 
 if __name__ == "__main__":
@@ -51,4 +17,8 @@ if __name__ == "__main__":
     add_holidays(df)
     add_weekdays(df)
     print(df.head(100))
-    # plot_features(dataframe)
+    plot_features(df)
+
+
+
+
---
+++ patternvisualize.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import datetime
+import holidays
+
+def plot_features(file):
+    df = file
+    print(df)
+    fig, axs = plt.subplots(5)
+    fig.suptitle("all plots")
+    # axs[0].figure(figsize=(16,8))
+    #axs[0].title('Traffic', fontsize = 14)
+    #axs[0].xlabel('Timer', fontsize= 12)
+    #axs[0].ylabel('Volum Tot', fontsize = 12)
+    axs[0].title.set_text("num cars after first hours")
+    axs[0].plot(df['Volum totalt'], label='TOT')
+    axs[0].plot(df['Volum til SNTR'], label='SNTR')
+    axs[0].plot(df['Volum til DNP'], label='DNP')
+    axs[0].legend(loc='best')
+    # plotting something else
+    axs[1].title.set_text("different directions compared")
+    axs[1].scatter(df['Volum til SNTR'],
+                   df['Volum til DNP'], s=2.5, alpha=0.03)
+    axs[1].axis('equal')
+    # plotting something else
+    axs[2].title.set_text("volum etter time")
+    axs[2].scatter(df['Volum totalt'], df['Fra_time'],
+                   cmap='gist_rainbow', s=0.5)
+    # plotting something else
+    axs[3].title.set_text("volum etter time med begge veier")
+    axs[3].scatter(df['Volum til DNP'], df['Fra_time'],
+                   cmap='gist_rainbow', s=2.2, c='blue', alpha=0.03)
+    axs[3].scatter(df['Volum til SNTR'], df['Fra_time'],
+                   cmap='gist_rainbow', s=2.2, c='green', alpha=0.03)
+    axs[4].plot(getweekavg("Volum til SNTR", df), label="SNTR")
+    axs[4].title.set_text("Total number of cars by weekday")
+    axs[4].plot(getweekavg("Volum til DNP", df), label="DNP")
+    axs[4].legend(loc='best')
+    # plotting something else
+    plt.show()
+
+def getweekavg(col, df):
+    week = [0]*7
+    for index, row in df.iterrows():
+        day = row['weekdays']
+        week[day] += row[col]
+    return week
