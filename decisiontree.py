81c208f1e1842d028bc2076e6ce834a6c5adfce8
Tobias E <tseilertsen@gmail.com>
2020-10-07 15:25:35 +0200

    working decision tree

--- decisiontree.py
+++ decisiontree.py
@@ -7,14 +7,15 @@ from sklearn.tree import *
 class DecisionTree:
 
     dt: DecisionTreeClassifier  = None
-
+    features = None
     def __init__(self, features, df):
         y = df["Volum til SNTR"]
+        self.features = features
         X = df[features]
-        dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-        dt.fit(X, y)
+        self.dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+        self.dt.fit(X, y)
     
     def test_accuracy(self, test):
         y_true = test["Volum til SNTR"]
-        y_pred = self.dt.predict(test["Volum til SNTR"])
+        y_pred = self.dt.predict(test[self.features])
         return mean_squared_error(y_true, y_pred)
--- main.py
+++ main.py
@@ -11,20 +11,29 @@ from decisiontree import *
 from sklearn.model_selection import train_test_split
 plt.style.use('bmh') 
 
-if __name__ == "__main__":
+def add_features():
+    '''adds features and removes unused columns from data'''
     df = (pd.read_csv("data.csv"))
     add_datetime(df)
     add_holidays(df)
     add_weekdays(df)
     add_month(df)
     remove_columns(df)
+    return df
 
+if __name__ == "__main__":
+
+    df = None
+    if(False):
+        df = add_features()
+        df.to_csv("modifieddata.csv")
+    else:
+        #read already modified data to save time
+        df = pd.read_csv("modifieddata.csv")
     train, test = train_test_split(df, test_size=0.20)
+
     #plot_features(df)
     print(df.head(100))
     features = [ 'Fra_time', 'is_holiday', 'weekdays', 'months' ]
     decision_tree = DecisionTree(features, train)
-    print("dt error", decision_tree.test_accuracy())
-
-
-
+    print("dt error", decision_tree.test_accuracy(test))
